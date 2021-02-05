ce8ca7ea5e1cee9dae5ebcccb88c5c0a65485466
Aksaule <aksaule124026@gmail.com>
2021-02-05 18:06:05 +0600

    Finish project

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+    
+
 def register(request):
 
     if request.method == 'POST':
@@ -23,6 +41,7 @@ def register(request):
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('user created')
+                return redirect('login')
             
         else: 
             print('Password not matching...')
@@ -31,3 +50,7 @@ def register(request):
     else:
         return render(request,'register.html')
     return render(request,'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect
